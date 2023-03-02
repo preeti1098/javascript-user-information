291fe9747fb4f4fa9cc4c3f5b74bd23d44bc9a20
preeti1098 <124133826+preeti1098@users.noreply.github.com>
2023-03-02 16:43:05 +0530

    new-work
    created-work

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+let username = prompt('What is your name ?')
+let posts = prompt('What is your total posts?')
+let followers = prompt('How many followers do you have ?')
+let following = prompt('How many following do you have ?')
+
+
+document.querySelector('.name').innerHTML = username 
+document.querySelector('.posts').innerHTML = posts 
+document.querySelector('.followers').innerHTML = followers
+document.querySelector('.following').innerHTML = following
